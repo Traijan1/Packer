18cc992a7f9ae36572820bc97ec24ef9b429fdcf
Traijan <weberalexander00@web.de>
2020-11-17 17:03:42 +0100

    Decode GUI hinzugefügt

--- Packer/MainWindow.xaml.cs
+++ Packer/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.IO;
 using Microsoft.Win32;
+using System.Windows.Input;
 
 namespace Packer {
     
@@ -12,6 +13,10 @@ namespace Packer {
 
         private void Button_ChoosePath(object sender, MouseButtonEventArgs e) {
             
+        }
+
+        private void Button_Decode(object sender, MouseButtonEventArgs e) {
+
         }
     }
 }
