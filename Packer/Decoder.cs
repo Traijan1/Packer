71038e1a4a9df930988dd2c11e81614bfaf9f5d1
Traijan <weberalexander00@web.de>
2020-11-17 15:59:46 +0100

    De- und Encoder hinzugefügt

---
+++ Packer/Decoder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packer {
+
+    /// <summary>
+    /// Kodiert Dateien zu .tom-Dateien
+    /// </summary>
+    public class Decoder {
+
+    }
+}
---
+++ Packer/Generals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packer {
+
+    /// <summary>
+    /// Allgemeine feste Werte
+    /// </summary>
+    public static class Generals {
+        public const String fileExt = ".tom";
+        public const char marker = '´';
+    }
+}
